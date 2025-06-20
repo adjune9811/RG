0c14c3920267ede9cafc9c63c7537ab01bdd563b
Ajay Rawat <ajayrawat111@gmail.com>
2025-06-20 18:12:43 +0530

    use spn

--- variables.tf
+++ variables.tf
@@ -1,8 +1,10 @@
+/*
 variable "subscription_id" {}
 variable "client_id" {}
 variable "client_secret" {}
 variable "tenant_id" {}
 
+*/
 variable "resource_group_name" {
   default = "myTFResourceGroup"
 }
